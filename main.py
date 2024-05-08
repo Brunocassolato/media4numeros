0584fdd86e3b9de38d2f0e7f8ed45c5f31c32181
Brunocassolato <166666428+Brunocassolato@users.noreply.github.com>
2024-05-08 18:00:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#essas são as 4 variaveis de numero
+n1=float(input("digite um numero:"))
+n2=float(input("digite outro numero:"))
+n3=float(input("digite outro numero:"))
+n4=float(input("digite outro numero:"))
+
+media=(n1+n2+n3+n4)/4
+
+print(f"valor da media: {media}") # no float usa . e não ,
